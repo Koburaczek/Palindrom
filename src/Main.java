96082e4a1fe159a4f59fb056ec6174ab63c3decc
student <student@vlab.local>
2023-11-21 13:12:07 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args)
+    {   String PodstawoweSlowo = ""; String OdwroconeSlowo = ""; Scanner in = new Scanner(System.in); System.out.println("Podaj Slowo: ");
+        PodstawoweSlowo = in.nextLine().toUpperCase(); int lenght = PodstawoweSlowo.length();
+        for(int i = lenght - 1 ;i>=0; i--) {OdwroconeSlowo = OdwroconeSlowo + PodstawoweSlowo.charAt(i);}
+        if (PodstawoweSlowo.equals(OdwroconeSlowo)){System.out.println("Jest Palindromem");}
+        else { System.out.println("Nie jest Palindromem");}
+    }
+}
