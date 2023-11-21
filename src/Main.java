dea8f171a97db1a63bb0f4ec0edc99ff09d34d66
Koburaczek <eryk.kruk45@gmail.com>
2023-11-21 13:34:19 +0100

    funkcja

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import java.util.*;
 public class Main {
-    public static void main(String[] args)
-    {   String PodstawoweSlowo, OdwroconeSlowo = "";
-        Scanner in = new Scanner(System.in);
-        System.out.println("Podaj Slowo: ");
-        PodstawoweSlowo = in.nextLine().toUpperCase();
-        int lenght = PodstawoweSlowo.length();
-        for(int i = lenght - 1 ;i>=0; i--)
+    public static void Palindrom(){
+        String PodstawoweSlowo, OdwroconeSlowo = ""; //Definiowanie zmiennych
+        Scanner in = new Scanner(System.in); //Funkcja uzyskująca dane od użytkownika
+        System.out.println("Podaj słowo"); //Prośba o podanie słowa do sprawdzenia
+        PodstawoweSlowo = in.nextLine().toUpperCase(); //Zmiana wielkości liter w słowie, aby uniknąć potencjalnych nieprawidłowości
+        int length = PodstawoweSlowo.length(); //Definiowanie zmiennej długości słowa
+        for ( int i = length - 1; i >= 0; i-- ) //Pętra, która odwraca słowo
             OdwroconeSlowo = OdwroconeSlowo + PodstawoweSlowo.charAt(i);
-        if (PodstawoweSlowo.equals(OdwroconeSlowo))
-            System.out.println("Jest Palindromem");
+        if (PodstawoweSlowo.equals(OdwroconeSlowo))//Sprawdzenie czy podane słowo jest identyczne jak otrzymane odwrócone słowo
+            System.out.println("Jest palindromem");//Informacja o wyniku pozytywnym
         else
-            System.out.println("Nie jest Palindromem");
+            System.out.println("Nie jest palindromem");//Informacja o wyniku negatywnym
     }
-}
+
+    public static void main(String[]args){
+        Palindrom();
+    }
+} 
+ 
